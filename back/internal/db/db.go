fd0cbaee23a668357cb9f00e4b9e247dedd25f12
SrIzan03 <izan.garciag@gmail.com>
2025-10-05 20:13:28 +0200

    feat: create the database module to connect to postgres and test the connection

---
+++ back/internal/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"back/internal/config"
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/stdlib"
+)
+
+func ConnectToDB(cfg config.PostgresConfig) (*sql.DB, error) {
+	source := fmt.Sprintf("postgresql://%s:%s@%s:%s/postgres", cfg.Username, cfg.Password, cfg.URL, cfg.Port)
+
+	db, err := sql.Open("pgx", source)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	ctx := context.Background()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database connection: %w", err)
+	}
+
+	return db, nil
+}
