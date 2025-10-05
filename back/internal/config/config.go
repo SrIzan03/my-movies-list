e150e93f646bcd168d84c9e127d3403f9f5288e9
SrIzan03 <izan.garciag@gmail.com>
2025-10-05 20:14:52 +0200

    feat: create the config package for loading the environment variables into the application

---
+++ back/internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"os"
+	// this will automatically load your .env file:
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Config struct {
+	Logs LogConfig
+	DB   PostgresConfig
+	Port string
+}
+
+type LogConfig struct {
+	Style string
+	Level string
+}
+
+type PostgresConfig struct {
+	Username string
+	Password string
+	URL      string
+	Port     string
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{
+		Port: os.Getenv("PORT"),
+		Logs: LogConfig{
+			Style: os.Getenv("LOG_STYLE"),
+			Level: os.Getenv("LOG_LEVEL"),
+		},
+		DB: PostgresConfig{
+			Username: os.Getenv("POSTGRES_USER"),
+			Password: os.Getenv("POSTGRES_PWD"),
+			URL:      os.Getenv("POSTGRES_URL"),
+			Port:     os.Getenv("POSTGRES_PORT"),
+		},
+	}
+
+	return cfg, nil
+}
