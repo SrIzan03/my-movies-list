0176f6bf63902c04dc109b279f9613d0b85ac611
SrIzan03 <izan.garciag@gmail.com>
2025-10-07 00:32:20 +0200

    feat: use the main context, create the mux and the auth service

--- back/cmd/api-server/main.go
+++ back/cmd/api-server/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"back/cmd/api-server/routes"
+	"back/internal/auth"
 	"back/internal/config"
 	"back/internal/db"
+	"context"
 	"database/sql"
 	"log"
+	"net/http"
 )
 
 func main() {
+	ctx := context.Background()
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		return
 	}
 
-	conn, err := db.ConnectToDB(cfg.DB)
+	conn, err := db.ConnectToDB(cfg.DB, ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,4 +28,9 @@ func main() {
 			log.Fatal(err)
 		}
 	}(conn)
+
+	mux := http.NewServeMux()
+	authService := auth.NewService(cfg, ctx)
+
+	routes.RegisterRoutes(mux, authService)
 }
